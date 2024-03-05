58125093e830132a0bc2a5a7fcb2eedc285efcf6
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-03-05 08:46:28 -0500

    wrong syntax

--- src/main.cpp
+++ src/main.cpp
@@ -67,7 +67,7 @@ class $modify(MyPlayLayer, PlayLayer) {
 			if (theLastCCNode->getChildrenCount() < 2) { continue; }
 			auto deathNode = typeinfo_cast<CCLabelBMFont*>(theLastCCNode->getChildren()->objectAtIndex(0));
 			if (deathNode == nullptr) { continue; }
-			#ifdef GEODE_IS_MACOS
+			#ifdef GEODE_IS_ANDROID GEODE_IS_WINDOWS
 				if (strcmp(deathNode->getFntFile(), "goldFont.fnt") != 0) { continue; } // avoid non-gold font HUDs
 			#endif
 			auto maybeOnDeathString = deathNode->getString();
