22d983c74122e83b74c634ed2537c6d3c0e5b2a9
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-10-31 13:17:54 -0400

    not a glorified alertlayertweaks advertisement

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Geode/modify/MenuLayer.hpp>
 #include <Geode/modify/PlayLayer.hpp>
+#include <Geode/modify/FLAlertLayer.hpp>
 #include <Geode/modify/PlayerObject.hpp>
 #include <filesystem>
 #include <iostream>
@@ -9,6 +10,8 @@
 #include <random>
 #include <regex>
 #include "Settings.hpp"
+
+#define PREFERRED_HOOK_PRIO (-2123456789)
 
 using namespace geode::prelude;
 
@@ -122,6 +125,21 @@ void forceEnableJustDont() {
 bool isNewBest(PlayLayer* pl) {
 	return pl->getCurrentPercentInt() > pl->m_level->m_normalPercent.value();
 }
+
+class $modify(MyFLAlertLayer, FLAlertLayer) {
+	static void onModify(auto & self)
+	{
+		(void) self.setHookPriority("FLAlertLayer::init", PREFERRED_HOOK_PRIO);
+	}
+	bool init(FLAlertLayerProtocol* delegate, char const* title, gd::string desc, char const* btn1, char const* btn2, float width, bool scroll, float height, float textScale) {
+		const std::string& titleAsString = title;
+		const std::string& descAsString = desc;
+		if (titleAsString != "\"New Best\" Text Font #" && !utils::string::contains(descAsString, "To use font numbers greater than 0: refer to the level editor for the font each number corresponds to, subtract that number by 1, and then put it here.") && !utils::string::contains(descAsString, "suggested by Saritahh") && !utils::string::contains(descAsString, "If using Oxygene One")) {
+			return FLAlertLayer::init(delegate, title, desc, btn1, btn2, width, scroll, height, textScale);
+		}
+		return FLAlertLayer::init(delegate, title, desc, btn1, btn2, 420.f, true, 320.f, 1.0f);
+	}
+};
 
 class $modify(MyMenuLayer, MenuLayer) {
 	struct Fields {
